073c9fec1c2e860fa517ff5ebf615573ac0c2fbe
JoeLametta <JoeLametta@users.noreply.github.com>
2016-07-25 10:04:43 +0200

    Fix previous commit
    Hopefully this one fixes a privately reported regression introduced
    with the previous commit.
    
    ```python
    TypeError: unsupported operand type(s) for %: 'NoneType' and 'tuple'
    ```

--- whatlogger/logger/whatcd.py
+++ whatlogger/logger/whatcd.py
@@ -112,10 +112,10 @@ class WhatLogger(result.Logger):
                              "(you may have a different pressing from the "
                              "one(s) in the database)" % arHeading)
             elif self._accuratelyRipped < nonHTOA:
+                accurateTracks = nonHTOA - self._accuratelyRipped
                 lines.append("%s Some tracks could not be verified as "
                              "accurate (%d/%d got no match)") % (
-                             arHeading, (nonHTOA - self._accuratelyRipped),
-                             nonHTOA)
+                             arHeading, accurateTracks, nonHTOA)
             else:
                 lines.append("%s All tracks accurately ripped" % arHeading)
 
